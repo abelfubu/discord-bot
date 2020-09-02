331a52be7b1a276ee129b2ce1f7ca94a375147e0
abelfubu <abelfubu@gmail.com>
2020-09-02 11:34:33 +0200

    add language

--- snippets/node.js
+++ snippets/node.js
@@ -8,7 +8,10 @@ const helpMsg = new discord_js_1.MessageEmbed()
     .setURL('https://nodejs.org/en/')
     .setDescription('Node code snippets')
     .setThumbnail('https://usefulangle.com/img/thumb/nodejs.png')
-    .addFields({ name: '$node cors', value: 'https://www.npmjs.com/package/cors' }, { name: '$node dotenv', value: 'https://www.npmjs.com/package/dotenv' }, { name: '$node jwt', value: 'https://www.npmjs.com/package/jsonwebtoken' }, { name: '$node multer', value: 'https://www.npmjs.com/package/multer' }, { name: '$node mysql', value: 'https://www.npmjs.com/package/mysql' })
+    .addFields({ name: '$node cors', value: 'https://www.npmjs.com/package/cors' }, { name: '$node dotenv', value: 'https://www.npmjs.com/package/dotenv' }, { name: '$node jwt', value: 'https://www.npmjs.com/package/jsonwebtoken' }, {
+    name: '$node language',
+    value: 'https://www.npmjs.com/package/languagedetect',
+}, { name: '$node multer', value: 'https://www.npmjs.com/package/multer' }, { name: '$node mysql', value: 'https://www.npmjs.com/package/mysql' })
     .setTimestamp()
     .setFooter('Javascript code snippets');
 exports.nodeSnippets = (snippet) => {
@@ -31,6 +34,36 @@ exports.node = {
     example: `\`\`\`javascript
   
     \`\`\``,
+    language: `\`\`\`javascript
+const LanguageDetect = require('languagedetect');
+const lngDetector = new LanguageDetect();
+ 
+// OR
+// const lngDetector = new (require('languagedetect'));
+ 
+console.log(lngDetector.detect('This is a test.'));
+ 
+/*
+  [ [ 'english', 0.5969230769230769 ],
+  [ 'hungarian', 0.407948717948718 ],
+  [ 'latin', 0.39205128205128204 ],
+  [ 'french', 0.367948717948718 ],
+  [ 'portuguese', 0.3669230769230769 ],
+  [ 'estonian', 0.3507692307692307 ],
+  [ 'latvian', 0.2615384615384615 ],
+  [ 'spanish', 0.2597435897435898 ],
+  [ 'slovak', 0.25051282051282053 ],
+  [ 'dutch', 0.2482051282051282 ],
+  [ 'lithuanian', 0.2466666666666667 ],
+  ... ]
+*/
+ 
+// Only get the first 2 results
+console.log(lngDetector.detect('This is a test.', 2));
+ 
+/*
+  [ [ 'english', 0.5969230769230769 ], [ 'hungarian', 0.407948717948718 ] ]
+*/\`\`\``,
     cors: `\`\`\`javascript
 const cors = require('cors');
 
